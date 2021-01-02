a865f40ba81cd9c0f1d93444a6e1cdfbc8d82d22
spiritbro1 <rinoakbr@gmail.com>
2021-01-02 13:33:11 +0700

    selesai

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  mode: 'universal',
+  ssr: true,
   server: {
     port: 8080, // default: 3000
     host: '0.0.0.0' // default: localhost
